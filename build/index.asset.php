7caf4b307c0c5ec95ba1074140c9ce5c127e7845
Mitsui <todays.mitsui@gmail.com>
2024-05-22 00:00:22 +0900

    .

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '39c2dae1d40dc06f29b6');
--- build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'a707e22b19e9f2af15c9');
