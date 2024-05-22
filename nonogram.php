67209691c2e5cd5e6cf93d769365f5422e3848df
Mitsui <todays.mitsui@gmail.com>
2024-05-22 23:50:47 +0900

    v0.2.0

--- nonogram.php
+++ nonogram.php
@@ -4,10 +4,10 @@
  * Description:       Example block scaffolded with Create Block tool.
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.1.0
+ * Version:           0.2.0
  * Author:            The WordPress Contributors
- * License:           GPL-2.0-or-later
- * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * License:           MIT
+ * License URI:       https://opensource.org/license/mit
  * Text Domain:       nonogram
  *
  * @package CreateBlock
