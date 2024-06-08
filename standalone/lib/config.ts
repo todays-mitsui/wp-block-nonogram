500491c665972fafffc96ebcc6c7399e128671af
todays_mitsui <todays_mitsui@hatena.ne.jp>
2024-06-08 17:31:12 +0900

    calcLayout

---
+++ standalone/lib/config.ts
@@ -0,0 +1,9 @@
+export const MIN_PADDING = 8; // px
+
+export const MAX_FONT_SIZE = 20; // px
+
+export const FONT_SIZE_RATIO = 0.45; // fontSize / cellSize
+
+export const HORIZONTAL_PADDING_RATIO = 0.15; // padding / fontSize
+
+export const VERTICAL_PADDING_RATIO = 0.25; // padding / fontSize
